f7358b319f7c0156379c54dd1dfc4c0de3279448
Shaked Winder <aesh2003@gmail.com>
2020-08-11 14:58:40 +0300

    Created a print array function

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+
+void print_array(const int* array, const size_t size)
+ {
+    size_t i = 0;
+    for (i = 0; i < size; ++i) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+
 int main() {
-    printf("Hello, World!\n");
+    int array[] = {0,1,2,3,4};
+    print_array(array,5);
     return 0;
 }
